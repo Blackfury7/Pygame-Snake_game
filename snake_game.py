c47675727abf0ab1c2256c460f9fa901e5eba3db
divyanshu <divyanshusharmabb07@gmail.com>
2021-02-18 00:47:01 +0530

    moving the snake: created x1_change & y1_change to update x & y

--- snake_game.py
+++ snake_game.py
@@ -3,26 +3,56 @@
 #https://www.pygame.org/docs/tut/PygameIntro.html
 
 import pygame
-pygame.init()
-dis = pygame.display.set_mode((400, 300))
 
-pygame.display.set_caption('Snake game')
+pygame.init()
 
 blue = (0, 0, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
 white = (255, 255, 255)
+black = (0, 0, 0)
+
+dis = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Snake game')
 
 game_over = False
+
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
 
 while not game_over:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			game_over = True
-		# print(event) #prints out all the actions that take place on the screen
 
-	pygame.draw.rect(dis, green, [200, 150, 10, 10])
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				x1_change = -10
+				y1_change = 0
+			elif event.key == pygame.K_RIGHT:
+				x1_change = 10
+				y1_change = 0
+			elif event.key == pygame.K_UP:
+				y1_change = -10
+				x1_change = 0
+			elif event.key == pygame.K_DOWN:
+				y1_change = 10
+				x1_change = 0
+
+	x1 += x1_change
+	y1 += y1_change
+	dis.fill(black)
+	pygame.draw.rect(dis, white, [x1, y1, 10, 10])
+
 	pygame.display.update()
+
+	clock.tick(30)
 
 pygame.quit()
 quit()
